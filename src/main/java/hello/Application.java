e2ca78beb053f055a7a6ceee0d3888213743b699
ashishblr2k <ashishblr2k@gmail.com>
2017-04-21 07:57:41 +0530

    first commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by agup101 on 4/20/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
